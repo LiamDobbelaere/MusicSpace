f8fbcc61580d6b5f0e7526c4ddd5bc390350bc98
Tom Dobbelaere <tom.dobbelaere@student.howest.be>
2018-06-11 21:13:04 +0200

    More worldbuilding

---
+++ Assets/Game Assets/Scripts/Utility/ScrollTexture.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollTexture : MonoBehaviour {
+    private Material material;
+    public Vector2 offsetStep;
+    public int materialId;
+
+	// Use this for initialization
+	void Start () {
+        material = GetComponent<Renderer>().materials[materialId];
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        material.mainTextureOffset += offsetStep * Time.deltaTime;
+    }
+}
